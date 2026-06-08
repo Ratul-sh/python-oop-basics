92fd29e01a42dbc512adb977adcafaa6fede50e1
Ratul-sh <hamim2117026@studd.kuet.ac.bd>
2026-06-09 02:58:01 +0600

    Reworked  with add_demographic method, made the syntex correct.

--- city_manager.py
+++ city_manager.py
@@ -1,4 +1,4 @@
-class city:
+class City:
     def __init__(self, name, country, facilities = None, demographics = None):
         self.name = name
         self.country = country
@@ -12,7 +12,8 @@ class city:
             self.demographics = demographics
     def add_facility(self, facility_name):
         self.facilities.append(facility_name)
-    def add_demographics(self, group_names, population):
-        self.demographics.update(group_names, population)
-        
+    def add_demographic(self, group_name, population):
+        self.demographics[group_name] = population 
+    def city_summery(self):
+        return f"City Name: {self.name}\n\nFacilities: {self.facilities}\n\nDemographics: {self.demographics}"
         
